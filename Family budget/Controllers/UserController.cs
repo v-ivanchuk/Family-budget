9ec862469d7480f6949940860d46caaa822933b2
Vitalii <v.ivanchuk1313@gmail.com>
2021-01-25 15:57:56 +0200

    Added user controller with necessary views

---
+++ Family budget/Controllers/UserController.cs
@@ -0,0 +1,143 @@
+﻿using Family_budget.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Family_budget.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly BudgetContext userContext;
+
+        public UserController(BudgetContext budgetContext)
+        {
+            userContext = budgetContext;
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = userContext.Users;
+            return View(users);
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            if(id <= 0)
+            {
+                return NotFound();
+            }
+
+            var user = await userContext.Users.FirstOrDefaultAsync(user => user.Id == id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(User user)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(user.Name))
+                {
+                    userContext.Users.Add(user);
+                    await userContext.SaveChangesAsync();
+                    return View("Index", userContext.Users);
+                }
+                return View("Index", userContext.Users);
+            }
+            catch
+            {
+                return View("Index", userContext.Users);
+            }
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var user = await userContext.Users.FirstOrDefaultAsync(user => user.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(User user)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(user.Name))
+                {
+                    userContext.Users.Update(user);
+                    await userContext.SaveChangesAsync();
+                    return View("Details", user);
+                }
+                return View("Index", userContext.Users);
+            }
+            catch
+            {
+                return View("Index", userContext.Users);
+            }
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await userContext.Users.FirstOrDefaultAsync(user => user.Id == id);
+
+            if(user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(User user)
+        {
+            try
+            {
+                userContext.Users.Remove(user);
+                await userContext.SaveChangesAsync();
+                return View("Index", userContext.Users);
+            }
+            catch
+            {
+                return View("Index", userContext.Users);
+            }
+        }
+    }
+}
--- Family budget/Models/User.cs
+++ Family budget/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace Family_budget.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public ICollection<UserExpense> UserExpenses { get; set; }
     }
 }
