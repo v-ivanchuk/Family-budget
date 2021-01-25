37d89e645e270c34cacdc0c6132e99b70584e3b3
Vitalii <v.ivanchuk1313@gmail.com>
2021-01-25 10:11:17 +0200

    Added User and UserExpense models

---
+++ Family budget/Models/UserExpense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Family_budget.Models
+{
+    public class UserExpense
+    {
+        public int Id { get; set; }
+        public decimal Expense { get; set; }
+        public DateTime CountDate { get; set; }
+    }
+}
