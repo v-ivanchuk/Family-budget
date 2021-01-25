18527fa4f993d5d51fd2b71dbe37050ea1d20d1e
Vitalii <v.ivanchuk1313@gmail.com>
2021-01-25 09:20:53 +0200

    Add project files.

---
+++ Family budget/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Family_budget.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
